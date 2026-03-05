1d8cd9942b06716b11f421fd7ff22c6d85100a08
ohh-losser <1243708162@qq.com>
2026-03-05 20:44:31 +0800

    3-5

--- src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -3,7 +3,6 @@ package com.nowcoder.community.controller;
 import com.nowcoder.community.service.AlphaService;
 import com.nowcoder.community.util.CommunityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
